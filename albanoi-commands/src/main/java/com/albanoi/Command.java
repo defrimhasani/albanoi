8ce4d8ff884279ce3e7b39ec65b357ae422fa3c5
defrimhasani <defrimhsn@gmail.com>
2023-01-09 18:39:09 +0100

    add command

---
+++ albanoi-commands/src/main/java/com/albanoi/Command.java
@@ -0,0 +1,7 @@
+package com.albanoi;
+
+/**
+ * A command is a request to update the state of the system
+ */
+public interface Command {
+}
---
+++ albanoi-commands/src/test/java/com/albanoi/CommandTest.java
@@ -0,0 +1,23 @@
+package com.albanoi;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CommandTest {
+    static class CreateUserCommand implements Command {
+        private String username;
+        public CreateUserCommand(String username){
+            this.username = username;
+        }
+    }
+
+    @Test
+    void verifyCommandCreation(){
+        var createUserCommand = new CreateUserCommand("US");
+        Assertions.assertInstanceOf(Command.class, createUserCommand);
+    }
+
+
+
+
+}
