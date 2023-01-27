430ca647ef012149e05916211f54fe5953082185
Hasani, Defrim <defrim.hasani@bottomline.com>
2023-01-27 22:12:24 +0000

    init documentation

--- albanoi-documentation/src/pages/index.tsx
+++ albanoi-documentation/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Docusaurus Tutorial - 5mins ⏱️
           </Link>
         </div>
       </div>
